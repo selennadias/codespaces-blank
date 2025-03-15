85cc87bf93ca3567e576e2d9870c2f9b56fa9a82
Elenna Dias <61871256+selennadias@users.noreply.github.com>
2025-03-15 12:55:31 +0000

    first commit

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+#Vamos receber dois  dados diferentes do usuário e concatena-los do usuário
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenada = info1 + " " +info2
+
+print("A informações concatenada são: ",info_concatenada)
